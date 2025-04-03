5c5b89332518e4136d50dd4787e09c3724436f29
Sanan <sananahmad909@gmail.com>
2025-04-03 23:16:01 +0500

    Most Frequent Number in an array - JavaScript

---
+++ MostFrequent.js
@@ -0,0 +1,26 @@
+function mostFrequentNumber(arr) {
+  let frequencyMap = {};
+
+  let count = 1;
+
+  let mostFrequent = arr[0];
+  let maxCount = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    if (frequencyMap[num]) {
+      frequencyMap[num] = frequencyMap[num] + 1;
+    } else {
+      frequencyMap[num] = 1;
+    }
+
+    if (frequencyMap[num] > maxCount) {
+      maxCount = frequencyMap[num];
+      mostFrequent = num;
+    }
+  }
+
+  return mostFrequent;
+}
+let arr = [1, 3, 3, 1, 1, 0, 2, 3, 3, 4];
+console.log(mostFrequentNumber(arr));
