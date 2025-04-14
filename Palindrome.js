ce8cb5d529d5c3336139d6314e0f55d79ff79f3e
Sanan <sananahmad909@gmail.com>
2025-04-14 20:01:23 +0500

    Check whether a string is a Palindrome

---
+++ Palindrome.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+  let len = str.length - 1;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - i]) {
+      return "Not a Palindrome";
+    }
+    return "String is a Palindrome";
+  }
+}
+
+const ok = "madam";
+console.log(palindrome(ok));
